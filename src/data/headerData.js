4b0548e588d236ac7145d68583018f7c96076b5a
imranabdisalan7 <agmraan@gmail.com>
2024-08-28 00:25:30 +0300

    public access

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Imran abdisalan',
+    name: 'Imran A.salan',
     title: "Web Developer",
     desciption:"Life's journey is a winding road filled with discoveries. Embrace each twist and turn, savoring the experiences and lessons along the way. Remember, it's not just about reaching a destination—it's about the journey  itself",
-    image: '',
+    image: 'https://media.istockphoto.com/id/473185460/photo/never-stop-learning-written-in-chalk-the-o-is-a-stopwatch.jpg?s=612x612&w=0&k=20&c=TLBXFGU2ro7tB1RO1pU-hsIJXUfqP5st4ZHWAkFtCIY=',
     resumePdf: resume
 }
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,12 +11,12 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'AnimeDatabase',
+        projectName: 'Animation Application',
         projectDesc: '',
         tags: ['', 'HTML' ,'CSS', 'Javascript'],
-        code: 'https://github.com/sreerag-rajan/historythroughmovies',
-        demo: 'https://historythroughmovies.pythonanywhere.com/',
-        image: "src/assets/svg/projects/menesota.png"
+        code: 'https://github.com/imraac/anime-app',
+        demo: 'https://anime-app-plum.vercel.app/',
+       
     },
     {
         id: 2,
@@ -25,35 +25,27 @@ export const projectsData = [
         tags: ['HTML', 'CSS','Javascript'],
         code: 'https://github.com/imraac/complete',
         demo: 'https://complete-ten.vercel.app/',
-        image: "src/assets/svg/projects/menesota.png"
+       
     },
     {
         id: 3,
-        projectName: 'Educative Clone',
-        projectDesc: 'This project is the clone of Educative.io. Built as part of Project week, my contributions include the home page(both with user and without user), course information page and checkout page. The rest of the pages and features were built by other members of my team',
-        tags: ['React', 'Material UI', 'Express', "MongoDB","NodeJS"],
-        code: 'https://github.com/sreerag-rajan/educative-clone',
-        demo: 'https://educative-clone.vercel.app/',
-        image: "https://user-images.githubusercontent.com/85482640/161571651-35ab8e4e-dc57-4834-bafc-9485766f5b8a.png"
+        projectName: 'LMSystem',
+        projectDesc: '',
+        tags: ['React', 'Python', 'Flask', "PostgreSQL","JScript"],
+        code: 'https://github.com/imraac/LMS-backend',
+        demo: 'https://backup-xi.vercel.app/',
+        
     },
     {
         id: 4,
         projectName: 'Reliance Digital Clone - Backend Integration',
-        projectDesc: 'This is the clone of reliancedigital.in. Features I worked on the Product Catalogue pages and also on some minor parts on other pages as well',
-        tags: ['EJS', 'Express', 'MongoDB', 'Node.js'],
-        code: 'https://github.com/sreerag-rajan/Reliance-Digital-Clone-U4-',
-        demo: 'https://reliance-digital-clone.herokuapp.com/',
-        image: "https://miro.medium.com/max/875/1*qRNfRbuHCDC1ct0chLZoWA.png"
+        projectDesc: 'Fullstack Timesheet App built with React ,sqlite & FastAPI',
+        tags: ['FastAPI', 'Sqlite', 'React', ''],
+        code: 'https://github.com/imraac/timesheet',
+        demo: 'https://timesheet-psi.vercel.app/',
+      
     },
-    {
-        id: 5,
-        projectName: 'Reliance Digital Clone - Frontend',
-        projectDesc: 'This project is the clone of Reliance Digital built as a part of a project week. It simulates the functionality of an e-commerce website from landing page to checkout. My contribution was the product catalog page arriving from the navbar, as well as leading the team.',
-        tags: ['HTML', 'CSS', 'Javascript'],
-        code: 'https://github.com/iamrituyadav/Reliance_digital',
-        demo: 'https://reliance-digital-clone.herokuapp.com/',
-        image: "https://miro.medium.com/max/875/1*qRNfRbuHCDC1ct0chLZoWA.png"
-    },
+   
     
 ]
 
